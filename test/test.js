9ad8f0b5475d934f5b62a7099639b4f05a781b1c
Max-PC\Max <mko98@hotmail.nl>
2017-06-20 00:20:28 +0200

    test

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ const request = require('supertest');
 const server = require('../server');
 const expect = require('chai').expect;
 
-describe('Login API', function() {
+describe('Correct login API', function() {
     it('Should success if credential is valid', function(done) {
         request(server)
             .post('/api/v1/login')
@@ -18,3 +18,54 @@ describe('Login API', function() {
             .end(done);
     });
 });
+
+describe('Incorrect login API', function() {
+    it('Should success if credential is invalid', function(done) {
+        request(server)
+            .post('/api/v1/login')
+            .set('Accept', 'application/json')
+            .set('Content-Type', 'application/json')
+            .send({ username: 'wronguser', password: 'wrongpass' })
+            .expect(401)
+            .expect('Content-Type', /json/)
+            .expect(function(response) {
+                expect(response.body).to.include({"error": "Invalid credentials, bye"});
+                expect(response.body).to.be.an('object');
+            })
+            .end(done);
+    });
+});
+
+// describe('Correct register API', function() {
+//     it('Should success if credential is not in database', function(done) {
+//         request(server)
+//             .post('/api/v1/register')
+//             .set('Accept', 'application/json')
+//             .set('Content-Type', 'application/json')
+//             .send({ username: 'nieuwegebruiker', password: 'nieuwegebruiker' })
+//             .expect(500)
+//             .expect('Content-Type', /json/)
+//             .expect(function(response) {
+//                 expect(response.body).not.to.be.empty;
+//                 expect(response.body).to.be.an('object');
+//             })
+//             .end(done);
+//     });
+// });
+//
+// describe('Incorrect register API', function() {
+//     it('Should success if credential is already in database', function(done) {
+//         request(server)
+//             .post('/api/v1/register')
+//             .set('Accept', 'application/json')
+//             .set('Content-Type', 'application/json')
+//             .send({ username: 'test', password: 'test' })
+//             .expect(500)
+//             .expect('Content-Type', /json/)
+//             .expect(function(response) {
+//                 expect(response.body).to.include({"Error": "Deze gebruiker bestaat al"});
+//                 expect(response.body).to.be.an('object');
+//             })
+//             .end(done);
+//     });
+// });

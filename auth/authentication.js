a4a8609e63a6765b7102eecdabdd9029175aed72
Max Koning <maxkoning@casema.nl>
2017-06-20 11:14:20 +0200

    niet in commentaar

--- auth/authentication.js
+++ auth/authentication.js
@@ -1,44 +1,44 @@
-// var settings = require('../config.json');
-// const moment = require('moment');
-// const jwt = require('jwt-simple');
+var settings = require('../config.json');
+const moment = require('moment');
+const jwt = require('jwt-simple');
+
 //
-// //
-// // Encode (van username naar token)
-// //
-// function encodeToken(username) {
-//     const playload = {
-//         exp: moment().add(60, 'minutes').unix(),
-//         iat: moment().unix(),
-//         sub: username
-//     };
-//     return jwt.encode(playload, settings.secretkey);
-// }
+// Encode (van username naar token)
 //
-// //
-// // Decode (van token naar username)
-// //
-// function decodeToken(token, cb) {
+function encodeToken(username) {
+    const playload = {
+        exp: moment().add(60, 'minutes').unix(),
+        iat: moment().unix(),
+        sub: username
+    };
+    return jwt.encode(playload, settings.secretkey);
+}
+
 //
-//     try {
-//         const payload = jwt.decode(token, settings.secretkey);
+// Decode (van token naar username)
 //
-//         // Check if the token has expired. To do: Trigger issue in db ..
-//         const now = moment().unix();
-//
-//         // Check if the token has expired
-//         if (now > payload.exp) {
-//             console.log('Token has expired.');
-//         }
-//
-//         // Return
-//         cb(null, payload);
-//
-//     } catch(err) {
-//         cb(err, null);
-//     }
-// }
-//
-// module.exports = {
-//     encodeToken,
-//     decodeToken
-// };
+function decodeToken(token, cb) {
+
+    try {
+        const payload = jwt.decode(token, settings.secretkey);
+
+        // Check if the token has expired. To do: Trigger issue in db ..
+        const now = moment().unix();
+
+        // Check if the token has expired
+        if (now > payload.exp) {
+            console.log('Token has expired.');
+        }
+
+        // Return
+        cb(null, payload);
+
+    } catch(err) {
+        cb(err, null);
+    }
+}
+
+module.exports = {
+    encodeToken,
+    decodeToken
+};
--- routes/routes_api_v1.js
+++ routes/routes_api_v1.js
@@ -5,97 +5,97 @@ var pool = require('../db/db_connector');
 var auth =  require('../auth/authentication');
 var bcrypt = require('bcryptjs');
 
-// router.all( new RegExp("[^(\/login|\/register)]"), function (req, res, next) {
-//
-//     console.log("VALIDATE TOKEN")
-//
-//     var token = (req.header('Token')) || '';
-//
-//     auth.decodeToken(token, function (err, payload) {
-//         if (err) {
-//             console.log('Error handler: ' + err.message);
-//             res.status((err.status || 401 )).json({error: new Error("Not authorised").message});
-//         } else {
-//             next();
-//         }
-//     });
-// });
-//
-// router.post('/login', function(req, res) {
-//
-//     var username = req.body.username || '';
-//     var password = req.body.password || '';
-//
-//     if (username != '' && password != '') {
-//         var query_str = {
-//             sql: query_str = 'SELECT password FROM customer WHERE username=?',
-//             values: [username],
-//             timeout: 2000
-//         }
-//
-//         pool.getConnection(function (error, connection) {
-//             if (error) {
-//                 throw error
-//             }
-//             connection.query(query_str, function (error, result, fields) {
-//                 connection.release();
-//                 if (error) {
-//                     throw error
-//                 }
-//
-//                 if (result.length > 0) {
-//                     bcrypt.compare(password, result[0].password, function (err, response) {
-//                         if (response === true) {
-//                             console.log("Correct ingevoerd password");
-//                             res.status(200).json({"token": auth.encodeToken(username), "username": username});
-//                         } else {
-//                             res.status(401).json({"error": "Invalid credentials, bye"})
-//                         }
-//                     });
-//                 } else {
-//                     res.status(401).json({"error": "Invalid credentials, bye"})
-//                 }
-//             });
-//         });
-//     }
-// });
-//
-// router.post('/register', function(req, res) {
-//
-//     var username = req.body.username || '';
-//     var password = req.body.password || '';
-//
-//     if (username != '' && password != '') {
-//         var hash = bcrypt.hashSync(password, 10);
-//         var query_str = {
-//             sql: 'INSERT INTO `customer` (username, password) VALUES (?, ?)',
-//             values: [username, hash],
-//             timeout: 2000 // 2secs
-//         };
-//
-//         pool.getConnection(function (error, connection) {
-//             if (error) {
-//                 throw error
-//             }
-//             connection.query(query_str, function (error, rows, fields) {
-//                 connection.release();
-//                 if (error) {
-//                     if (error.code === 'ER_DUP_ENTRY') {
-//                         res.status(200).json({"Error": "Deze gebruiker bestaat al"});
-//                         return;
-//                     } else {
-//                         throw error
-//                     }
-//                 }
-//                 console.log("Gebruiker aangemaakt in database");
-//                 console.log("Password opgeslagen als hash in database");
-//
-//                 // Generate JWT
-//                 res.status(200).json({"token": auth.encodeToken(username), "username": username});
-//             });
-//         });
-//     };
-// });
+router.all( new RegExp("[^(\/login|\/register)]"), function (req, res, next) {
+
+    console.log("VALIDATE TOKEN")
+
+    var token = (req.header('Token')) || '';
+
+    auth.decodeToken(token, function (err, payload) {
+        if (err) {
+            console.log('Error handler: ' + err.message);
+            res.status((err.status || 401 )).json({error: new Error("Not authorised").message});
+        } else {
+            next();
+        }
+    });
+});
+
+router.post('/login', function(req, res) {
+
+    var username = req.body.username || '';
+    var password = req.body.password || '';
+
+    if (username != '' && password != '') {
+        var query_str = {
+            sql: query_str = 'SELECT password FROM customer WHERE username=?',
+            values: [username],
+            timeout: 2000
+        }
+
+        pool.getConnection(function (error, connection) {
+            if (error) {
+                throw error
+            }
+            connection.query(query_str, function (error, result, fields) {
+                connection.release();
+                if (error) {
+                    throw error
+                }
+
+                if (result.length > 0) {
+                    bcrypt.compare(password, result[0].password, function (err, response) {
+                        if (response === true) {
+                            console.log("Correct ingevoerd password");
+                            res.status(200).json({"token": auth.encodeToken(username), "username": username});
+                        } else {
+                            res.status(401).json({"error": "Invalid credentials, bye"})
+                        }
+                    });
+                } else {
+                    res.status(401).json({"error": "Invalid credentials, bye"})
+                }
+            });
+        });
+    }
+});
+
+router.post('/register', function(req, res) {
+
+    var username = req.body.username || '';
+    var password = req.body.password || '';
+
+    if (username != '' && password != '') {
+        var hash = bcrypt.hashSync(password, 10);
+        var query_str = {
+            sql: 'INSERT INTO `customer` (username, password) VALUES (?, ?)',
+            values: [username, hash],
+            timeout: 2000 // 2secs
+        };
+
+        pool.getConnection(function (error, connection) {
+            if (error) {
+                throw error
+            }
+            connection.query(query_str, function (error, rows, fields) {
+                connection.release();
+                if (error) {
+                    if (error.code === 'ER_DUP_ENTRY') {
+                        res.status(200).json({"Error": "Deze gebruiker bestaat al"});
+                        return;
+                    } else {
+                        throw error
+                    }
+                }
+                console.log("Gebruiker aangemaakt in database");
+                console.log("Password opgeslagen als hash in database");
+
+                // Generate JWT
+                res.status(200).json({"token": auth.encodeToken(username), "username": username});
+            });
+        });
+    };
+});
 
 router.get('/films/:filmid', function(req, res){
     var id = req.params.filmid;
